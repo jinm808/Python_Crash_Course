b137bad3f1c5c43bacb32daf88ef89151e84e672
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-08-18 16:04:18 -0700

    Rename directory

---
+++ chapter_2_variables/favorite_number.py
@@ -0,0 +1,10 @@
+# 2-9: Favorite Number
+"""
+Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals your favorite number. Print that message.
+"""
+
+fav_num = 42
+msg = f"My favorite number is {fav_num}."
+
+print(msg)
