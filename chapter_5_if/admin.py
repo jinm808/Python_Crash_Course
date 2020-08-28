cb23afe73a7d65a8246c73f991e424fe2dca9ed1
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-08-28 16:09:41 -0700

    Minor tweaks

--- chapter_5_if/admin.py
+++ chapter_5_if/admin.py
@@ -12,4 +12,4 @@ for username in usernames:
     if username == 'admin':
         print("Hello admin, would you like to see a status report?")
     else:
-        print("Hello " + username + ", thank you for logging in again!")
+        print(f"Hello {username}, thank you for logging in again!")
