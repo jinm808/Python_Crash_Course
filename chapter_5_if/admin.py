403511b134cda2eb7e01cf76331ef82845f148e9
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-08-28 16:10:49 -0700

    Minor tweaks

--- chapter_5_if/admin.py
+++ chapter_5_if/admin.py
@@ -6,10 +6,10 @@
 # If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 # Otherwise, print a generic greeting, such as Hello Eric, thank you for loggin in again.
 
-usernames = ['eric', 'willie', 'admin', 'erin', 'ever']
+usernames = ['Eric', 'wiLLie', 'admin', 'eRin', 'ever']
 
 for username in usernames:
-    if username == 'admin':
+    if username.lower() == 'admin':
         print("Hello admin, would you like to see a status report?")
     else:
         print(f"Hello {username}, thank you for logging in again!")
