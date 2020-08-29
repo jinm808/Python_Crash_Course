cac2e2b507615cf87b683e35637f06e2267e0dcb
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-08-29 14:16:53 -0700

    Add places.py

---
+++ chapter_6_dictionaries/places.py
@@ -0,0 +1,16 @@
+# 6-9: Favorite Places
+# Make a dictionary called favorite_places. 
+# Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. 
+# To make this exericse a bit more interesting, ask some friends to name a few of their favorite places. 
+# Loop through the dictionary, and print each person’s name and their favorite places.
+
+favorite_places = {
+    'eric': ['bear mountain', 'death valley', 'tierra del fuego'],
+    'erin': ['hawaii', 'iceland'],
+    'ever': ['mt. verstovia', 'the playground', 'south carolina']
+    }
+
+for person, places in favorite_places.items():
+  print(f'\n{person.title()}\'s favorite places are:')
+  for place in places:
+    print(f'- {place.title()}')
