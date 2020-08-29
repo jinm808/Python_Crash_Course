857845b39eecb84c91192db1ddbfefbdaa46ca01
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-08-29 14:07:56 -0700

    Minor tweaks

--- chapter_6_dictionaries/rivers.py
+++ chapter_6_dictionaries/rivers.py
@@ -1,6 +1,5 @@
 # 6-5: Rivers
-# Make a dictionary containing three major rivers and the country each river runs through. 
-# One key-value pair might be 'nile': 'egypt'.
+# Make a dictionary containing three major rivers and the country each river runs through. One key-value pair might be 'nile': 'egypt'.
 rivers = {
     'nile': 'egypt',
     'mississippi': 'united states',
@@ -12,14 +11,13 @@ rivers = {
 # Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
 for k, v in rivers.items():
   print(f'{k.title()} runs through {v.title()}.')
-print('\n')
 
 # Use a loop to print the name of each river included in the dictionary.
+print("\nThe following rivers are included in this data set:")
 for key in rivers.keys():
   print(key.title())
-print('\n')
 
 # Use a loop to print the name of each country included in the dictionary.
+print("\nThe following countries are included in this data set:")
 for value in rivers.values():
   print(value.title())
-print('\n')
