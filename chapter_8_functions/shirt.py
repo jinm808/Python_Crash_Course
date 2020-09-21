9158e3a3212268b150dfa0b1080d43e3825ba066
Kate Malakhova <64453840+kmalakhova@users.noreply.github.com>
2020-09-20 19:20:43 -0700

    Delete shirt.py

--- chapter_8_functions/shirt.py
+++
@@ -1,9 +0,0 @@
-def make_shirt(size, text):
-  """Summarize the shirt that's going to be made."""
-  print(f'I\'m going to make a {size} size t-shirt. It will say, "{text}".')
-
-# Call the function using positional arguments.
-make_shirt('Large', 'I love Python!')
-
-# Call the function using keyword arguments.
-make_shirt(size = 'Medium', text = 'Readability counts')
