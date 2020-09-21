19d7cbbb1f9961f5b825998098c3d1bb768c1e24
Ekaterina Malakhova <kgrekhova@gmail.com>
2020-09-20 19:54:02 -0700

    Add large_shirts.py: Default value practice

---
+++ chapter_8_functions/large_shirts.py
@@ -0,0 +1,7 @@
+def make_shirt(size='large', text='I love Python!'):
+  """Summarize the shirt that's going to be made."""
+  print(f'I\'m going to make a {size} size t-shirt. \nIt will say, "{text}".')
+
+make_shirt()
+make_shirt(size = 'medium')
+make_shirt('small', 'Programmers are loopy.')
